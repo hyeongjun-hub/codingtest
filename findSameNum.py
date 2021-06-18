cbc60eef6a591a5a246fca7904a320cc6413eedb
Hyeongjun Jo <chogudwns@naver.com>
2021-06-18 22:54:21 +0900

    find_same_number

--- findSameNum.py
+++ findSameNum.py
@@ -7,6 +7,20 @@ def find_same_number(some_list):
         if result[some_list[i]] >= 2:
             return some_list[i]
 
+#
+# def find_same_number(some_list):
+#     # 필요한 경우, start와 end를 옵셔널 파라미터로 만들어도 됩니다.
+#     # 코드를 쓰세요
+#     temp_list = list();
+#     for value in some_list[:-1]:
+#         if value in temp_list:
+#             return value
+#         else:
+#             if value == some_list[-1]:
+#                 return value
+#             else:
+#                 temp_list.append(value)
+
 
 # 중복되는 수 ‘하나’만 리턴합니다.
 print(find_same_number([1, 4, 3, 5, 3, 2]))
