433f222dd31785a9480e2a2419f9f32d5d60d4cc
hyeongjun <chogudwns@naver.com>
2021-11-06 01:59:03 +0900

    11/6

--- minseong.py
+++ minseong.py
@@ -67,28 +67,36 @@
 
 
 
-alpha = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
-}
-buffer, result = "", ""
-for letter in "one2two3three4567nine8eight":
-    buffer += letter
-    if buffer in alpha:
-        result += alpha.get(buffer)
-        buffer = ""
-    if buffer.isdigit():
-        result += buffer
-        buffer = ""
+# alpha = {
+#     'one': '1',
+#     'two': '2',
+#     'three': '3',
+#     'four': '4',
+#     'five': '5',
+#     'six': '6',
+#     'seven': '7',
+#     'eight': '8',
+#     'nine': '9'
+# }
+# buffer, result = "", ""
+# for letter in "one2two3three4567nine8eight":
+#     buffer += letter
+#     if buffer in alpha:
+#         result += alpha.get(buffer)
+#         buffer = ""
+#     if buffer.isdigit():
+#         result += buffer
+#         buffer = ""
+# print(result)
 
-print(result)
+
+def solution(s):
+    answer = 0
+    a = ['zero', 'one', 'two', 'three', 'four',
+         'five', 'six', 'seven', 'eight', 'nine']
+    for i in a:
+        s = s.replace(i, str(a.index(i)))
+    return int(s)
 
 
 
