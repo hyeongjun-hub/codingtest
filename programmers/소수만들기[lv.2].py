86e5072965d865a3eaa28537dd7ccee9e2df780d
hyeongjun <chogudwns@naver.com>
2021-11-16 23:18:16 +0900

    11/17 소수만들기

--- programmers/소수만들기[lv.2].py
+++ programmers/소수만들기[lv.2].py
@@ -1,12 +1,14 @@
+from itertools import combinations as cb
+
 
 def solution(nums):
-    from itertools import combinations as cb
     answer = 0
     for a in cb(nums, 3):
         can = sum(a)
         for j in range(2, can):
-            if can%j==0:
+            if can % j == 0:
                 break
         else:
             answer += 1
+
     return answer
